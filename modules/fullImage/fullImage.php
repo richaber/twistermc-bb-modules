4050c0886dd3f66252e167df20b3594cca4c0d6c
Richard Aber <raber@nerdery.com>
2018-06-11 13:01:28 -0500

    A little code cleanup, use proper text domain

--- modules/fullImage/fullImage.php
+++ modules/fullImage/fullImage.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * BBFullImage Class file.
+ *
+ * @package TwisterMcBBModules
+ */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -6,211 +11,191 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * @class BBFullImage
+ * Class BBFullImage
  */
 class BBFullImage extends FLBuilderModule {
 
-    /**
-     * Constructor function for the module. You must pass the
-     * name, description, dir and url in an array to the parent class.
-     *
-     * @method __construct
-     */
-    public function __construct()
-    {
-        parent::__construct(array(
-            'name'          => __('Full Image', 'fl-builder'),
-            'description'   => __('Full Width Image', 'fl-builder'),
-            'category'		=> __('Advanced Modules', 'fl-builder'),
-            'dir'           => TMC_BB_DIR . 'fullImage/',
-            'url'           => TMC_BB_URL . 'fullImage/',
-            'editor_export' => true, // Defaults to true and can be omitted.
-            'enabled'       => true, // Defaults to true and can be omitted.
-        ));
+	/**
+	 * Constructor function for the module. You must pass the
+	 * name, description, dir and url in an array to the parent class.
+	 *
+	 * @method __construct
+	 */
+	public function __construct() {
 
-        /**
-         * Use these methods to enqueue css and js already
-         * registered or to register and enqueue your own.
-         */
-        // Already registered
-        $this->add_css('font-awesome');
-    }
+		parent::__construct(
+			array(
+				'name'          => __( 'Full Image', 'tmcbbm' ),
+				'description'   => __( 'Full Width Image', 'tmcbbm' ),
+				'category'      => __( 'Advanced Modules', 'tmcbbm' ),
+				'dir'           => TMCBBM_DIR . 'modules/fullImage/',
+				'url'           => TMCBBM_URL . 'modules/fullImage/',
+			)
+		);
 
-    /**
-     * Use this method to work with settings data before
-     * it is saved. You must return the settings object.
-     *
-     * @method update
-     * @param $settings {object}
-     */
-    public function update($settings)
-    {
-        $settings->textarea_field .= ' - this text was appended in the update method.';
+		$this->add_css( 'font-awesome' );
+	}
 
-        return $settings;
-    }
+	/**
+	 * Register the module and its form settings.
+	 *
+	 * What's implied, but not specifically stated, is that Beaver Builder itself handles instantiation on demand.
+	 * An unusual side effect of that approach, is that hooks in constructor appear to fire more than once.
+	 *
+	 * @action init
+	 */
+	public static function register() {
+		self::register_module();
+	}
 
-    /**
-     * This method will be called by the builder
-     * right before the module is deleted.
-     *
-     * @method delete
-     */
-    public function delete()
-    {
+	/**
+	 * Register the module and it's form settings with Beaver Builder.
+	 *
+	 * @see \FLBuilderModel::register_module()
+	 */
+	public static function register_module() {
 
-    }
-
-    /**
-     * Add additional methods to this class for use in the
-     * other module files such as preview.php, frontend.php
-     * and frontend.css.php.
-     *
-     *
-     * @method example_method
-     */
-    public function example_method()
-    {
-
-    }
+		/**
+		 * Register the module and its form settings.
+		 */
+		FLBuilder::register_module(
+			'BBFullImage',
+			array(
+				'general' => array(
+					'title'    => __( 'Full Image', 'tmcbbm' ),
+					'sections' => array(
+						'general'      => array(
+							'title'  => __( 'Photo Settings', 'tmcbbm' ),
+							'fields' => array(
+								'photo_field'    => array(
+									'type'  => 'photo',
+									'label' => __( 'Photo', 'tmcbbm' ),
+								),
+								'forceImageSize' => array(
+									'type'    => 'select',
+									'label'   => __( 'Force Image to Full Width', 'tmcbbm' ),
+									'default' => 'true',
+									'options' => array(
+										'true'  => __( 'Yes', 'tmcbbm' ),
+										'false' => __( 'No', 'tmcbbm' ),
+									),
+									'toggle'  => array(
+										'false' => array(
+											'fields' => array( 'imageAlign' ),
+										),
+									),
+								),
+								'imageAlign'     => array(
+									'type'    => 'select',
+									'label'   => __( 'Image Align', 'tmcbbm' ),
+									'default' => 'left',
+									'options' => array(
+										'left'   => __( 'Left', 'tmcbbm' ),
+										'center' => __( 'Center', 'tmcbbm' ),
+										'right'  => __( 'Right', 'tmcbbm' ),
+									),
+								),
+								'showCaption'    => array(
+									'type'    => 'select',
+									'label'   => __( 'Show Caption', 'tmcbbm' ),
+									'default' => 'false',
+									'options' => array(
+										'true'  => __( 'Yes', 'tmcbbm' ),
+										'false' => __( 'No', 'tmcbbm' ),
+									),
+									'toggle'  => array(
+										'true' => array(
+											'sections' => array( 'captionStyle' ),
+										),
+									),
+								),
+							),
+						),
+						'captionStyle' => array(
+							'title'  => __( 'Caption Style', 'tmcbbm' ),
+							'fields' => array(
+								'captionSize'            => array(
+									'type'        => 'text',
+									'size'        => 3,
+									'label'       => __( 'Caption Text Size', 'tmcbbm' ),
+									'default'     => 14,
+									'description' => 'px',
+								),
+								'captionAlign'           => array(
+									'type'    => 'select',
+									'label'   => __( 'Text Align', 'tmcbbm' ),
+									'default' => 'center',
+									'options' => array(
+										'left'   => __( 'Left', 'tmcbbm' ),
+										'center' => __( 'Center', 'tmcbbm' ),
+										'right'  => __( 'Right', 'tmcbbm' ),
+									),
+								),
+								'captionColor'           => array(
+									'type'    => 'color',
+									'label'   => __( 'Caption Text Color', 'tmcbbm' ),
+									'default' => 'ffffff',
+								),
+								'captionBackgroundColor' => array(
+									'type'       => 'color',
+									'label'      => __( 'Caption Background Color', 'tmcbbm' ),
+									'default'    => '333333',
+									'show_reset' => true,
+								),
+								'captionPadding'         => array(
+									'type'        => 'text',
+									'size'        => 3,
+									'label'       => __( 'Caption Padding', 'tmcbbm' ),
+									'default'     => 5,
+									'description' => 'px',
+								),
+							),
+						),
+					),
+				),
+				'link'    => array(
+					'title'    => __( 'Link', 'tmcbbm' ),
+					'sections' => array(
+						'general-link' => array(
+							'title'  => __( 'Link', 'tmcbbm' ),
+							'fields' => array(
+								'linkImage' => array(
+									'type'    => 'select',
+									'label'   => __( 'Link Image', 'tmcbbm' ),
+									'default' => 'false',
+									'options' => array(
+										'true'  => __( 'Yes', 'tmcbbm' ),
+										'false' => __( 'No', 'tmcbbm' ),
+									),
+									'toggle'  => array(
+										'true' => array(
+											'sections' => array( 'linkdetails' ),
+										),
+									),
+								),
+							),
+						),
+						'linkdetails'  => array(
+							'title'  => __( 'Link Details' ),
+							'fields' => array(
+								'linkurl'    => array(
+									'type'  => 'link',
+									'label' => __( 'Link', 'tmcbbm' ),
+								),
+								'linktarget' => array(
+									'type'    => 'select',
+									'label'   => __( 'Target', 'tmcbbm' ),
+									'default' => '_self',
+									'options' => array(
+										'_self'  => __( 'Same Window', 'tmcbbm' ),
+										'_blank' => __( 'New Window', 'tmcbbm' ),
+									),
+								),
+							),
+						),
+					),
+				),
+			)
+		);
+	}
 }
-
-/**
- * Register the module and its form settings.
- */
-FLBuilder::register_module('BBFullImage', array(
-    'general'       => array( // Tab
-        'title'         => __('Full Image', 'fl-builder'), // Tab title
-        'sections'      => array( // Tab Sections
-            'general'       => array( // Section
-                'title'         => __('Photo Settings', 'fl-builder'), // Section Title
-                'fields'        => array( // Section Fields
-                    'photo_field'     => array(
-                        'type'          => 'photo',
-                        'label'         => __('Photo', 'fl-builder'),
-                    ),
-                    'forceImageSize'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Force Image to Full Width', 'fl-builder'),
-                        'default'       => 'true',
-                        'options'       => array(
-                            'true'      => __('Yes', 'fl-builder'),
-                            'false'      => __('No', 'fl-builder')
-                        ),
-                        'toggle'        => array(
-                            'false'      => array(
-                                'fields'      => array( 'imageAlign' ),
-                            ),
-                        )
-                    ),
-                    'imageAlign'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Image Align', 'fl-builder'),
-                        'default'       => 'left',
-                        'options'       => array(
-                            'left'      => __('Left', 'fl-builder'),
-                            'center'      => __('Center', 'fl-builder'),
-                            'right'      => __('Right', 'fl-builder')
-                        )
-                    ),
-                    'showCaption'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Show Caption', 'fl-builder'),
-                        'default'       => 'false',
-                        'options'       => array(
-                            'true'      => __('Yes', 'fl-builder'),
-                            'false'      => __('No', 'fl-builder')
-                        ),
-                        'toggle'        => array(
-                            'true'      => array(
-                                'sections'      => array( 'captionStyle' ),
-                            ),
-                        )
-                    ),
-                )
-            ),
-            'captionStyle'       => array( // Section
-                'title'         => __('Caption Style', 'fl-builder'), // Section Title
-                'fields'        => array( // Section Fields
-                    'captionSize'   => array(
-                        'type'          => 'text',
-                        'size'          => 3,
-                        'label'         => __('Caption Text Size', 'fl-builder'),
-                        'default'       => 14,
-                        'description'   => 'px'
-                    ),
-                    'captionAlign'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Text Align', 'fl-builder'),
-                        'default'       => 'center',
-                        'options'       => array(
-                            'left'      => __('Left', 'fl-builder'),
-                            'center'      => __('Center', 'fl-builder'),
-                            'right'      => __('Right', 'fl-builder')
-                        )
-                    ),
-                    'captionColor'   => array(
-                        'type'          => 'color',
-                        'label'         => __('Caption Text Color', 'fl-builder'),
-                        'default'       => 'ffffff'
-                    ),
-                    'captionBackgroundColor'   => array(
-                        'type'          => 'color',
-                        'label'         => __('Caption Background Color', 'fl-builder'),
-                        'default'       => '333333',
-                        'show_reset'    => true
-                    ),
-                    'captionPadding'   => array(
-                        'type'          => 'text',
-                        'size'          => 3,
-                        'label'         => __('Caption Padding', 'fl-builder'),
-                        'default'       => 5,
-                        'description'   => 'px'
-                    ),
-                )
-            )
-        )
-    ),
-    'link'       => array( // Tab
-        'title'         => __('Link', 'fl-builder'), // Tab title
-        'sections'      => array( // Tab Sections
-            'general-link'       => array( // Section
-                'title'         => __('Link', 'fl-builder'), // Section Title
-                'fields'        => array( // Section Fields
-                    'linkImage'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Link Image', 'fl-builder'),
-                        'default'       => 'false',
-                        'options'       => array(
-                            'true'      => __('Yes', 'fl-builder'),
-                            'false'      => __('No', 'fl-builder')
-                        ),
-                        'toggle'        => array(
-                            'true'      => array(
-                                'sections'      => array( 'linkdetails' ),
-                            ),
-                        )
-                    ),
-                )
-            ),
-            'linkdetails'       => array( // Section
-                'title'         => __('Link Details'), // Section Title
-                'fields'        => array( // Section Fields
-                    'linkurl'   => array(
-                        'type'          => 'link',
-                        'label'         => __('Link', 'fl-builder'),
-                    ),
-                    'linktarget'   => array(
-                        'type'          => 'select',
-                        'label'         => __('Target', 'fl-builder'),
-                        'default'       => '_self',
-                        'options'       => array(
-                            '_self'      => __('Same Window', 'fl-builder'),
-                            '_blank'      => __('New Window', 'fl-builder'),
-                        )
-                    ),
-                )
-            )
-        )
-    ),
-));
